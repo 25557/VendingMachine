79b36ef7fc408bf65e8ae66effcfa46daed822ca
25557 <43168595+25557@users.noreply.github.com>
2018-11-07 09:17:59 +0100

    Add files via upload
    V1.1
    
    Wanneer je boven de 100 cent of onder de 85 cent zit gaat hij meteen stoppen.

--- src/Vending.java
+++ src/Vending.java
@@ -7,50 +7,40 @@ public class Vending {
         System.out.println("MILKYWAY: €0,95");
         System.out.println("TWIX: €1,00");
         System.out.println("KITKAT: €0,85");
-
+        
         int SNICKERS = 90;
         int MILKYWAY = 95;
         int KITKAT = 85;
         int TWIX = 100;
         int tien, vijf;
-        int choice = 0;
+        int keuze = 0;
         int geld = 0;
-        int amount = 0;
+        int hoeveel = 0;
 
             System.out.println("Selecteer jouw product.");
             Scanner input = new Scanner(System.in);
-            choice = input.nextInt();
-
+            keuze = input.nextInt();
 
             System.out.println("Hoeveel cent heeft u?");
 
             geld = input.nextInt();
 
-            if (geld < 85) {
-                System.out.println("Je moet boven de 85 cent invullen.");
-            }
-            if (geld > 100) {
-                System.out.println("Je moet onder de 100 cent invullen.");
-            }
-            if ((geld < 85) || (geld > 100)) {
-                //
-            }
+        if ((geld >= 85) && (geld <= 100)) {
 
-
-            switch (choice) {
+            switch (keuze) {
                 case 1:
 
-                    System.out.println("Je hebt " + geld + "cent ingeworpen.");
+                    System.out.println("Je hebt " + geld + " cent ingeworpen.");
                     System.out.println("Je hebt SNICKERS gekozen. - .90");
-                    amount = geld - SNICKERS;
+                    hoeveel = geld - SNICKERS;
 
-                    System.out.println("Jouw wisselgeld: " + amount + "cent");
-                    System.out.println(" Je krijgt dit terug: ");
+                    System.out.println("Jouw wisselgeld: " + hoeveel + "cent");
+                    System.out.println("Je krijgt dit terug: ");
 
-                    amount = amount % 25;
-                    tien = amount / 10;
-                    amount = amount % 10;
-                    vijf = amount / 5;
+                    hoeveel = hoeveel % 25;
+                    tien = hoeveel / 10;
+                    hoeveel = hoeveel % 10;
+                    vijf = hoeveel / 5;
 
                     System.out.println(tien + " stuiver(s)");
                     System.out.println(vijf + " kwartje(s)");
@@ -61,14 +51,15 @@ public class Vending {
                 case 2:
 
                     System.out.println("Je hebt " + geld + " cent ingeworpen.");
-                    System.out.println("Jouw wisselgeld: " + amount + " cent");
-                    amount = geld - MILKYWAY;
-                    System.out.println(" Je krijgt dit terug: ");
+                    System.out.println("Je hebt MILKYWAY gekozen. - .95");
+                    hoeveel = geld - MILKYWAY;
+                    System.out.println("Jouw wisselgeld: " + hoeveel + " cent");
+                    System.out.println("Je krijgt dit terug: ");
 
-                    amount = amount % 25;
-                    tien = amount / 10;
-                    amount = amount % 10;
-                    vijf = amount / 5;
+                    hoeveel = hoeveel % 25;
+                    tien = hoeveel / 10;
+                    hoeveel = hoeveel % 10;
+                    vijf = hoeveel / 5;
 
                     System.out.println(tien + " stuiver(s)");
                     System.out.println(vijf + " kwartje(s)");
@@ -80,14 +71,14 @@ public class Vending {
 
                     System.out.println("je hebt " + geld + " cent ingeworpen.");
                     System.out.println("Je hebt TWIX gekozen. - 1,00");
-                    amount = geld - TWIX;
-                    System.out.println("Jouw wisselgeld: " + amount + " cent.");
-                    System.out.println(" Je krijgt dit terug: ");
+                    hoeveel = geld - TWIX;
+                    System.out.println("Jouw wisselgeld: " + hoeveel + " cent.");
+                    System.out.println("Je krijgt dit terug: ");
 
-                    amount = amount % 25;
-                    tien = amount / 10;
-                    amount = amount % 10;
-                    vijf = amount / 5;
+                    hoeveel = hoeveel % 25;
+                    tien = hoeveel / 10;
+                    hoeveel = hoeveel % 10;
+                    vijf = hoeveel / 5;
 
                     System.out.println(tien + " stuiver(s)");
                     System.out.println(vijf + " kwartje(s)");
@@ -99,14 +90,14 @@ public class Vending {
 
                     System.out.println("Je hebt: " + geld + " cent ingeworpen.");
                     System.out.println("Je hebt KITKAT gekozen -.85");
-                    amount = geld - KITKAT;
-                    System.out.println("Jouw wisselgeld: " + amount + " cent");
+                    hoeveel = geld - KITKAT;
+                    System.out.println("Jouw wisselgeld: " + hoeveel + " cent");
                     System.out.println("Je krijgt dit terug: ");
 
-                    amount = amount % 25;
-                    tien = amount / 10;
-                    amount = amount % 10;
-                    vijf = amount / 5;
+                    hoeveel = hoeveel % 25;
+                    tien = hoeveel / 10;
+                    hoeveel = hoeveel % 10;
+                    vijf = hoeveel / 5;
 
                     System.out.println(tien + " stuiver(s)");
                     System.out.println(vijf + " kwartje(s)");
@@ -118,5 +109,16 @@ public class Vending {
 
                     System.out.println("Je moet een keuze maken tussen 1 en 4.");
             }
+
+        } else {
+            if (geld < 85) {
+                System.out.println("Je moet boven de 85 cent invullen.");
+
+            }
+            if (geld > 100) {
+                System.out.println("Je moet onder de 100 cent invullen.");
+            }
+        }
+
     }
 }
